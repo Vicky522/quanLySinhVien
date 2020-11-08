b62623c812d6cad6ce808a0c5395a81561e53439
Hai <2000caothehai@gmail.com>
2020-11-08 16:17:49 +0700

    add Mon hoc

--- models/LopHoc.js
+++ models/LopHoc.js
@@ -2,6 +2,8 @@ var LopHoc = function () {
   this.danhSachSinhVien = [];
   //   userA
   this.moTa = "day la lop frontend 54";
+  // User B
+  this.tenLopHoc="front-end 54";
   this.layDanhSachSinhVien = function () {
     return this.layDanhSachSinhVien;
   };
---
+++ models/MonHoc.js
@@ -0,0 +1,8 @@
+var MonHoc=function(){
+    this.tenMonHoc="";
+    this.maMonHoc="";
+    this.xuatThongTinMonHoc=function(){
+        console.log(`Ma mon hoc :${this.maMonHoc}`);
+        console.log(`Ten mon hoc :${this.tenMonHoc}`);
+    }
+}
