36e2a298b8cd759f3e488b85d8d338b158a534ed
Vicky522 <vothanhvan522@gmail.com>
2020-11-08 15:51:41 +0700

    a

--- models/LopHoc.js
+++ models/LopHoc.js
@@ -1,5 +1,7 @@
 var LopHoc = function () {
   this.danhSachSinhVien = [];
+  //   userA
+  this.moTa = "day la lop frontend 54";
   this.layDanhSachSinhVien = function () {
     return this.layDanhSachSinhVien;
   };
