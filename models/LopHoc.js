c838919b90e9ab876ea96b34bd213e579cbb136e
Hai <2000caothehai@gmail.com>
2020-11-08 16:09:53 +0700

    add ten lop hoc

--- models/LopHoc.js
+++ models/LopHoc.js
@@ -1,11 +1,7 @@
 var LopHoc = function () {
   this.danhSachSinhVien = [];
-<<<<<<< HEAD
   //   userA
   this.moTa = "day la lop frontend 54";
-=======
-  this.tenSinhVien="fronten";
->>>>>>> 7877085b6f348b752f8ea52b424dd2a4d9855246
   this.layDanhSachSinhVien = function () {
     return this.layDanhSachSinhVien;
   };
